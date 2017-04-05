4090e48fb4c2ffa3c5bece6ac0135118ec31b166
Ramiro Eduardo Solis Castillo <a301633@uach.mx>
2017-04-05 17:15:16 -0600

    Problem Fix

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+$(document).ready(function(){
+   
+   
+});
+
 $("#get").click(function(){
         
-        $.getJSON("https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=hola",function(data){
-        console.log(data);     
-         
-    }); 
-        
-    });
-      
+        $.ajax({
+            url: "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=hola",
+            jsonp: "callback",
+            dataType: "jsonp",
+            success: function(data){
+                console.log(data);
+            }});
+});
