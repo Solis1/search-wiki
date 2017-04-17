c1f9a5dcc4cb044d3c278490b1fa296fadf97b24
Ramiro Eduardo Solis Castillo <a301633@uach.mx>
2017-04-16 20:40:56 -0600

    Page complete

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 $(document).ready(function(){
    
-   
-});
-
-$("#get").click(function(){
+    $("input").keydown(function(event){
         
-        $.ajax({
-            url: "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=hola",
+        if(event.which == 13){
+            $(".element").remove();
+            
+            var search = $("input").val();
+            
+            $.ajax({
+            url: "https://es.wikipedia.org/w/api.php?format=json&action=query&generator=search&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="+search,
             jsonp: "callback",
             dataType: "jsonp",
             success: function(data){
-                console.log(data);
+                
+                results = data.query.pages;
+                
+                $.each(results, function(i, item) {
+                    $("#content").append("<div class='element'><a class='page "+i+"' href='https://en.wikipedia.org/?curid="+i+"'></a></div>")  
+                    $(".page."+i).append("<h1 class='title'>"+item.title+"</h1>");
+                    if(item.hasOwnProperty("thumbnail")){
+                        
+                    $(".page."+i).append("<img src='"+item.thumbnail.source+"'/>");
+                    }
+                    $(".page."+i).append("<h2 class='extract'>"+item.extract+"</h2>");
+                    
+                    
+                }); 
             }});
+        }
+    });
 });
+
